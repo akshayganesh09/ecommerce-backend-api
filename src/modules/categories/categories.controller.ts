bb07a130bf030c9727e31d5d2b2d6c4ab2502135
Akshay Ganesh <iakshayganesh98@gmail.com>
2026-03-11 18:50:21 +0530

    category crud

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -13,4 +13,44 @@ export class CategoryController {
         next(error);
     }
   }
+
+  async findAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await service.getAllCategory();
+
+      res.status(200).json({ message: "", success: true, category: result })
+    } catch (error) {
+      next(error);
+    }
+  }
+
+    async findById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await service.getCategoryById(req.params.id as string);
+
+      res.status(200).json({ message: "", success: true, category: result })
+    } catch (error) {
+      next(error);
+    }
+  }
+
+    async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await service.updateCategory(req.params.id as string, req.body);
+
+      res.status(200).json({ message: "", success: true, category: result })
+    } catch (error) {
+      next(error);
+    }
+  }
+
+    async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await service.deleteCategory(req.params.id as string);
+
+      res.status(200).json({ message: "", success: true, category: result })
+    } catch (error) {
+      next(error);
+    }
+  }
 }
--- src/modules/categories/categories.repository.ts
+++ src/modules/categories/categories.repository.ts
@@ -4,4 +4,34 @@ export class CategoryRepository {
     async create(data: any) {
         return prisma.category.create({ data });
     }
+
+    async findAll() {
+        return prisma.category.findMany({
+            include: {
+                products: true
+            }
+        });
+    }
+
+    async findById(id: string) {
+        return prisma.category.findUnique({
+            where: { id },
+            include: {
+                products: true
+            }
+        });
+    }
+
+    async update(id: string, data: any) {
+        return prisma.category.update({
+            where: { id },
+            data
+        });
+    }
+
+    async delete(id: string) {
+        return prisma.category.delete({
+            where: { id }
+        });
+    }
 };
